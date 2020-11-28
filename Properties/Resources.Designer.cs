2a16d345140042236830072ecf6a78d5f54118c5
윤창호 <elca2003@paran.com>
2020-11-28 12:43:24 +0900

    어셈블리명 변경

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace YunDictionary.Properties {
+namespace YounDictionary.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace YunDictionary.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("YunDictionary.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("YounDictionary.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
